a07aac1da0a280cd48b158a7aad2f67d8d7149b7
7zhiqq <kfgmationg@gmail.com>
2025-10-20 12:19:56 +0800

    Modify MongoDB URI for security
    Updated MongoDB connection URI to use placeholders for username and password.

--- inventory_mgmt/db.py
+++ inventory_mgmt/db.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://kfgmationg_db_user:sevenzhiq@inventory-management.d9mq3xr.mongodb.net/?retryWrites=true&w=majority&appName=inventory-management"
+uri = "mongodb+srv://<username>:<password>@inventory-management.d9mq3xr.mongodb.net/?retryWrites=true&w=majority&appName=inventory-management"
 
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["market"]   # your database name
